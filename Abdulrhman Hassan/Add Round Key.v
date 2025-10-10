a3dbac3193e676802bb0d59cb170934318086c69
abdulrhmanshokier <abdulrhmanshokier107@gmail.com>
2025-10-10 17:48:35 +0300

    mahe the encryption process but i didn't try to run the files togther

---
+++ Abdulrhman Hassan/Add Round Key.v
@@ -0,0 +1,13 @@
+module Add_Round_Key 
+(
+    input      [127:0] plain_text,
+    input      [127:0] key,
+    output reg [127:0] resultant_state_array
+);
+
+assign resultant_state_array = plain_text ^ key;
+
+endmodule
+
+
+// I made it sequential but chatgpt told me its better if it done combinational
