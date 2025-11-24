af4926eab487690fe82d75d05b74b95a6c88d4bf
Abdelmouniem Semary <139631854+abdelmenammohamad@users.noreply.github.com>
2025-11-24 23:22:46 +0200

    fix syntax error

--- Iterative/Decryption/Decryptor/Top.v
+++ Iterative/Decryption/Decryptor/Top.v
@@ -5,7 +5,7 @@ module AES_Top #(parameter BLOCK_LENGTH = 128) (
     input  wire [BLOCK_LENGTH-1:0]  Cipher_Text,  // 128-bit input ciphertext
     input  wire [BLOCK_LENGTH-1:0]  RoundKey_i,   // Iterative round key (changes each round)
     input  wire [3:0]               Round_Number, // Current round number from FSM
-    output wire [BLOCK_LENGTH-1:0]  Plain_Text,   // 128-bit output plaintext
+    output wire [BLOCK_LENGTH-1:0]  Plain_Text   // 128-bit output plaintext
 );
 
     // Instantiate the AES Decryption core (iterative version)
