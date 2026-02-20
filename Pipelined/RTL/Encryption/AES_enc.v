cfd41689a1a9858bd2d5087c358429011b83c374
Abdulrahman-A-Rashad <abdulrahmanrashad9595@gmail.com>
2026-02-20 23:30:48 +0200

    copying the lower design into main repo

--- Pipelined/RTL/Encryption/AES_enc.v
+++ Pipelined/RTL/Encryption/AES_enc.v
@@ -10,21 +10,15 @@ module AES_enc #(parameter BLOCK_LENGTH = 128)
     output                        valid_out   //valid output data
 );
 
-
     reg [127:0] k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10;
-    wire [BLOCK_LENGTH-1:0]  r1_out ,r2_out, r3_out, r4_out, r5_out, 
-                             r6_out, r7_out, r8_out, r9_out, r10_out;
-
-
+    wire [BLOCK_LENGTH-1:0]    r0_out, r1_out ,r2_out, r3_out, r4_out, 
+                             r5_out, r6_out, r7_out, r8_out, r9_out, r10_out;
     wire         key_en;
     wire [3:0]   round_counter;                      
     wire [127:0] current_key;
     
-
     // Pipeline Valid Logic
     reg [10:0] en_pipe; // Pipeline to track data valid through rounds
-
-
 
     always @(posedge clk) begin
         if (!rst) begin
@@ -35,8 +29,6 @@ module AES_enc #(parameter BLOCK_LENGTH = 128)
             en_pipe <= {en_pipe[9:0], enable};
         end
     end
-
-
 
     // ── Individual FF registers ──
     always @(posedge clk) begin
@@ -103,19 +95,20 @@ module AES_enc #(parameter BLOCK_LENGTH = 128)
                              .current_key(current_key));
 
     // ── Round Instances ──
-    round_1        round1  (.clk(clk), .rst(rst), .IN(IN), .KEY(k0), .KEY1(k1) , .OUT(r1_out));
-    rounds_2_to_9  round2  (.clk(clk), .rst(rst), .IN(r1_out), .KEY(k2), .OUT(r2_out));
-    rounds_2_to_9  round3  (.clk(clk), .rst(rst), .IN(r2_out), .KEY(k3), .OUT(r3_out));
-    rounds_2_to_9  round4  (.clk(clk), .rst(rst), .IN(r3_out), .KEY(k4), .OUT(r4_out));
-    rounds_2_to_9  round5  (.clk(clk), .rst(rst), .IN(r4_out), .KEY(k5), .OUT(r5_out));
-    rounds_2_to_9  round6  (.clk(clk), .rst(rst), .IN(r5_out), .KEY(k6), .OUT(r6_out));
-    rounds_2_to_9  round7  (.clk(clk), .rst(rst), .IN(r6_out), .KEY(k7), .OUT(r7_out));
-    rounds_2_to_9  round8  (.clk(clk), .rst(rst), .IN(r7_out), .KEY(k8), .OUT(r8_out));
-    rounds_2_to_9  round9  (.clk(clk), .rst(rst), .IN(r8_out), .KEY(k9), .OUT(r9_out));
+    round_0        round0 (.clk(clk), .rst(rst), .IN(IN), .KEY(k0), .OUT(r0_out));
+    rounds_1_to_9  round1 (.clk(clk), .rst(rst), .IN(r0_out), .KEY(k1), .OUT(r1_out));
+    rounds_1_to_9  round2 (.clk(clk), .rst(rst), .IN(r1_out), .KEY(k2), .OUT(r2_out));
+    rounds_1_to_9  round3 (.clk(clk), .rst(rst), .IN(r2_out), .KEY(k3), .OUT(r3_out));
+    rounds_1_to_9  round4 (.clk(clk), .rst(rst), .IN(r3_out), .KEY(k4), .OUT(r4_out));
+    rounds_1_to_9  round5 (.clk(clk), .rst(rst), .IN(r4_out), .KEY(k5), .OUT(r5_out));
+    rounds_1_to_9  round6 (.clk(clk), .rst(rst), .IN(r5_out), .KEY(k6), .OUT(r6_out));
+    rounds_1_to_9  round7 (.clk(clk), .rst(rst), .IN(r6_out), .KEY(k7), .OUT(r7_out));
+    rounds_1_to_9  round8 (.clk(clk), .rst(rst), .IN(r7_out), .KEY(k8), .OUT(r8_out));
+    rounds_1_to_9  round9 (.clk(clk), .rst(rst), .IN(r8_out), .KEY(k9), .OUT(r9_out));
     round_10       round10 (.clk(clk), .rst(rst), .IN(r9_out), .KEY(k10), .OUT(r10_out));
 
     // Outputs 
     assign OUT = (valid_out)? r10_out : 128'b0;
-    assign valid_out = en_pipe[9]; // Output is valid when pipeline is full
+    assign valid_out = en_pipe[10]; // Output is valid when pipeline is full
 
 endmodule
--- Pipelined/RTL/Encryption/rounds_1_to_9.v
+++ Pipelined/RTL/Encryption/rounds_1_to_9.v
@@ -1,4 +1,4 @@
-module round_10 #(parameter BLOCK_LENGTH = 128)
+module rounds_1_to_9 #(parameter BLOCK_LENGTH = 128)
 (
     input                         clk,
     input                         rst,  
@@ -12,36 +12,40 @@ module round_10 #(parameter BLOCK_LENGTH = 128)
 
 wire [127:0] sub_out;
 wire [127:0] shft_out;
+wire [127:0] mix_out;
 wire [127:0] xor_out;
 
 
-sub_bytes  sub_10       (.IN(IN), .OUT(sub_out));
+sub_bytes   sub_1_to_9       (.IN(IN), .OUT(sub_out));
 
-shift_rows shft_10      (.IN(sub_out), .OUT(shft_out));
+shift_rows  shft_1_to_9      (.IN(sub_out), .OUT(shft_out));
 
-key_add    xor_with_k10 (.IN(shft_out), .KEY(KEY), .OUT(xor_out)); 
+mix_columns mix_1_to_9       (.IN(shft_out), .OUT(mix_out));
+
+key_add     xor_with_k1_to_9 (.IN(mix_out), .KEY(KEY), .OUT(xor_out)); 
+
+
+
 
 
 
 always@(posedge clk )
 begin
-    
+
         if(!rst)
         begin
             OUT <= 128'b0;
         end
 
         else
-       OUT <= xor_out; 
-    //    begin
-    //         if(enable)
-    //         begin   
-            
-    //         end 
-    //     end
+        OUT <= xor_out; 
+        // begin
+        //     if(enable)
+        //     begin   
+           
+        //     end 
+        // end
 end
-
-
 
 
 
