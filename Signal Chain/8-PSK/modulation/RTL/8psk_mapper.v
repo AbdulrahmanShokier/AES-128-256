fd6bb34dfbbbfb41d2473e3c47ac8afce51d1022
abdulrhmanshokier <abdulrhmanshokier107@gmail.com>
2026-04-25 15:32:25 +0300

    8psk finished and tested

--- Signal Chain/8-PSK/modulation/RTL/8psk_mapper.v
+++ Signal Chain/8-PSK/modulation/RTL/8psk_mapper.v
@@ -1,12 +1,12 @@
-module 8psk_mapper #(
+module psk8_mapper #(
     parameter DATA_WIDTH = 16,
     parameter no_bits    = 3,
     parameter FRAC_WIDTH = 14
 )
 (
-    input  wire                         clk_symbol,
-    input  wire                         rst,
-    input  wire                         valid_in,
+    input                               clk_symbol,
+    input                               rst,
+    input                               valid_in,
     input  wire        [no_bits-1 : 0]  data_in,    
 
     output reg  signed [DATA_WIDTH-1:0] I_out,
@@ -38,14 +38,14 @@ begin
         if (valid_in) 
         begin
             case(data_in)
-            3'b000 : I_out <= 16'b0;            Q_out <= 16'b0;
-            3'b001 : I_out <= SQRT2_INV;        Q_out <= SQRT2_INV;
-            3'b011 : I_out <= 16'b0;            Q_out <= one;
-            3'b010 : I_out <= NEG_SQRT2_INV;    Q_out <= SQRT2_INV;
-            3'b110 : I_out <= minus_one;        Q_out <= 16'b0;
-            3'b111 : I_out <= NEG_SQRT2_INV;    Q_out <= NEG_SQRT2_INV;
-            3'b101 : I_out <= 16'b0;            Q_out <= minus_one
-            3'b100 : I_out <= SQRT2_INV;        Q_out <= NEG_SQRT2_INV;
+            3'b000 : begin I_out <= one;              Q_out <= 16'b0;         end
+            3'b001 : begin I_out <= SQRT2_INV;        Q_out <= SQRT2_INV;     end
+            3'b011 : begin I_out <= 16'b0;            Q_out <= one;           end
+            3'b010 : begin I_out <= NEG_SQRT2_INV;    Q_out <= SQRT2_INV;     end
+            3'b110 : begin I_out <= minus_one;        Q_out <= 16'b0;         end
+            3'b111 : begin I_out <= NEG_SQRT2_INV;    Q_out <= NEG_SQRT2_INV; end
+            3'b101 : begin I_out <= 16'b0;            Q_out <= minus_one;     end
+            3'b100 : begin I_out <= SQRT2_INV;        Q_out <= NEG_SQRT2_INV; end
             endcase
     end
     end
